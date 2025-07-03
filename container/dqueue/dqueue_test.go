87c48d89a17700332eeb169d99780018cb395d4e
none <none@secret.com>
2025-07-03 11:03:21 +0000

    Update
    Signed-off-by: none <none@secret.com>

---
+++ container/dqueue/dqueue_test.go
@@ -0,0 +1,30 @@
+package dqueue
+
+import (
+	"testing"
+	"time"
+
+	"github.com/microsuite/go-toolkit/container/pqueue"
+)
+
+func TimeToS(t time.Time) int64 {
+	return t.UnixNano() / int64(time.Second)
+}
+
+func TestDelayQueue(t *testing.T) {
+	quit := make(chan struct{})
+
+	dq := NewDelayQueue(10)
+
+	go func() {
+		dq.Poll(quit, func() int64 {
+			return TimeToS(time.Now().UTC())
+		})
+	}()
+
+	item1 := &pqueue.Item{Value: "Item 1", Priority: 3}
+	item2 := &pqueue.Item{Value: "Item 2", Priority: 4}
+
+	dq.Add(item1, 3)
+	dq.Add(item2, 10)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/microsuite/go-toolkit
+
+go 1.23.7
